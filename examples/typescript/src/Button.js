c66f4560d9471b678af7fee9329305357acbe8fd
wilsson <wilsonft83@gmail.com>
2019-12-26 19:00:38 -0500

    fix: add regex for typescript support loader

--- examples/typescript/src/Button.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-exports.Button = function (_a) {
-    var children = _a.children;
-    return React.createElement("button", null, children);
-};
