f979d70d1e0ce858deb8c7d94ba97d0c10fafc76
wilsson <wilsonft83@gmail.com>
2020-08-08 18:44:26 -0500

    feat(@papyrum/ui-docs): add badge for status component

--- packages/papyrum-ui-docs/src/components/Components/styled.ts
+++ packages/papyrum-ui-docs/src/components/Components/styled.ts
@@ -7,19 +7,29 @@ import {
 } from '@papyrum/ui';
 
 export const Status = styled.div`
+  height: 18px;
+  line-height: 18px;
+  white-space: nowrap;
   width: max-content;
-  color: white;
-  padding: 2px 8px;
-  border-radius: 10px;
-  font-size: 14px;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 4px;
+  border-radius: 2px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
   ${props => props.status === 'ready' && css`
-    background-color: ${props => props.theme.inner.green};
+    background-color: #c6f6d5;
+    color: #22543d;
   `}
   ${props => props.status === 'development' && css`
-    background-color: ${props => props.theme.inner.skyblue};
+    background-color: #e9d8fd;
+    color: #44337a;
   `}
   ${props => props.status === 'deprecated' && css`
-    background-color: ${props => props.theme.inner.orange};
+    background-color: #fed7d7;
+    color: #822727;
   `}
 `;
 
